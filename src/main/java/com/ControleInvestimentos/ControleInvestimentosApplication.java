b630022c40917d0b15b422c3989694bd8ce50360
Eder Rodrigues <ederpr@gmail.com>
2020-12-07 21:50:45 -0300

    arquivo de configuracao

---
+++ src/main/java/com/ControleInvestimentos/ControleInvestimentosApplication.java
@@ -0,0 +1,13 @@
+package com.ControleInvestimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleInvestimentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleInvestimentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ControleInvestimentos/ControleInvestimentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ControleInvestimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleInvestimentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
